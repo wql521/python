bfd3c4d62fc3ca639bac47e0384e489e81276f5f
尘世烟雨客 <wql@wangqianlong.com>
2022-06-14 21:20:39 +0800

    6-14

--- test.py
+++ test.py
@@ -1,9 +1,8 @@
 if __name__ == '__main__':
-    s1={1,2,3,4,5}
-    s2={2,3,5,6}
-    s3={2,3,5}
-    s3.add(6)
-    s3.remove(2)
-    s3.clear()
-    print(s3)
+    def a(x,y):
+        x=99
+        y=99
+        return 'love'
 
+    b=a(1,2)
+    print(b)
