9b831f6d4e975885fa639161eef45fa214d95c4e
尘世烟雨客 <wql@wangqianlong.com>
2022-07-22 19:24:00 +0800

    7-22

---
+++ python控制键盘/源码（控制）.py
@@ -0,0 +1,36 @@
+if __name__ == '__main__':
+    import pyautogui
+    #获取鼠标位置
+    print(pyautogui.position())
+
+    #移动鼠标到指定位置
+    pyautogui.moveTo(23,478, duration=2)
+    #鼠标点击
+    pyautogui.click()
+
+    #键盘输入
+    pyautogui.typewrite('ni')
+    pyautogui.press('1')
+
+    pyautogui.typewrite('hao')
+    pyautogui.press('1')
+
+    pyautogui.typewrite('wo')
+    pyautogui.press('1')
+
+    pyautogui.typewrite('shi')
+    pyautogui.press('1')
+
+    pyautogui.typewrite('python')
+
+    pyautogui.typewrite('hengaoxing')
+    pyautogui.press('1')
+
+    pyautogui.typewrite('weinifuwu')
+    pyautogui.press('1')
+
+    pyautogui.press('enter')
+    pyautogui.press('enter')
+
+
+
